eaa8bb6f08074f46fdfac3344f89536875b32174
jjjkahn <31192156+jjjkahn@users.noreply.github.com>
2020-05-22 08:30:35 -0400

    Unity Configurations

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-    public class InMemoryRepository<T> where T : BaseEntity
+    public class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache = MemoryCache.Default;
         List<T> items;
@@ -18,7 +19,7 @@ namespace MyShop.DataAccess.InMemory
             //this gets the name of the class
             className = typeof(T).Name;
             items = cache[className] as List<T>;
-            if(items == null)
+            if (items == null)
             {
                 items = new List<T>();
             }
@@ -35,7 +36,7 @@ namespace MyShop.DataAccess.InMemory
         public void Update(T t)
         {
             T tToUpdate = items.Find(i => i.Id == t.Id);
-            if(tToUpdate != null)
+            if (tToUpdate != null)
             {
                 tToUpdate = t;
             }
@@ -63,7 +64,7 @@ namespace MyShop.DataAccess.InMemory
         public void Delete(string Id)
         {
             T tToDelete = items.Find(i => i.Id == Id);
-            if(tToDelete!= null)
+            if (tToDelete != null)
             {
                 items.Remove(tToDelete);
             }
