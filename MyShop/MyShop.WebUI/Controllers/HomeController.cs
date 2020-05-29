2bd5f8512d67edd1d59d4f242c47e5f42733c4b5
jjjkahn <31192156+jjjkahn@users.noreply.github.com>
2020-05-28 20:51:25 -0400

    Created a product filter

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Antlr.Runtime.Misc;
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -18,10 +19,22 @@ namespace MyShop.WebUI.Controllers
             context = productContext;
             productCategories = productCategoriesContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> products = context.Collection().ToList();
-            return View(products);
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+            if(Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+            ProductListViewModel model = new ProductListViewModel();
+            model.Products = products;
+            model.ProductCategories = categories;
+            return View(model);
         }
         public ActionResult Details(string Id)
         {
