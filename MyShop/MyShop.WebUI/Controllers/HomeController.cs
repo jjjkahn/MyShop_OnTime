c191ba57832627534fd023f72205d37a6a9859c2
jjjkahn <31192156+jjjkahn@users.noreply.github.com>
2020-05-28 20:12:52 -0400

    Addded a product list

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,12 +1,39 @@
-﻿using System.Web.Mvc;
+﻿using Antlr.Runtime.Misc;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web.Mvc;
 
 namespace MyShop.WebUI.Controllers
 {
+ 
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoriesContext)
+        {
+            context = productContext;
+            productCategories = productCategoriesContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
         }
 
         public ActionResult About()
